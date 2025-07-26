a7c94493bf1e6b23e103c1e0a2cd0f087cdfc7de
Pavel <gusevp220@gmail.com>
2025-07-26 20:03:41 +0300

    Optimization file downloads

--- images_cats.py
+++ images_cats.py
@@ -1,9 +1,10 @@
+import time
 import requests
 import os
 from data import yd_token
 import logging
 import json
-import urllib.parse
+from urllib.parse import quote, unquote
 from tqdm import tqdm
 from typing import Optional, Dict, List
 
@@ -27,13 +28,13 @@ class YandexAPI:
     """
 
     def __init__(self, ydtoken: str, folder_name: str = 'py-fpy-spd_132', progress: Optional[tqdm] = None) -> None:
-        self.ydtoken = ydtoken
+        self.__ydtoken = ydtoken
         self.folder_name = folder_name
         self.progress = progress
 
     @property
     def headers(self) -> dict:
-        return {'Authorization': f'OAuth {self.ydtoken}'}
+        return {'Authorization': f'OAuth {self.__ydtoken}'}
 
     def create_folder(self) -> None:
         """Создание папки на Яндекс диске"""
@@ -53,49 +54,37 @@ class YandexAPI:
             self.progress.set_description("Создание папки")
             self.progress.update(1)
 
-    def upload_file(self, local_file_path: str) -> Optional[Dict[str, int]]:
+    def upload_file(self, url_img: str) -> Optional[Dict[str, int]] | None:
         """Загрузка файла на Яндекс диск"""
-        logger.debug(f"Загрузка файла {local_file_path} в папку {self.folder_name} на Яндекс диске")
-        file_name = os.path.basename(local_file_path)
-        disk_path = f'{self.folder_name}/{file_name}'
+        logger.debug(f"Загрузка файла {unquote(url_img)} в папку {self.folder_name} на Яндекс диске")
+        self.file_name = os.path.basename(unquote(url_img))+'.jpg'
+        self.disk_path = f'{self.folder_name}/{self.file_name}'
         upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
-        params = {'path': disk_path, 'overwrite': 'true'}
-
-        response = requests.get(upload_url, headers=self.headers, params=params)
-        if response.status_code != 200:
-            logger.error(f"❌ Ошибка при получении ссылки для загрузки: {response.text}")
-            return None
-
-        href = response.json().get('href')
-        if not href:
-            logger.error("❌ Не удалось получить ссылку для загрузки")
-            return None
-
-        with open(local_file_path, 'rb') as file_data:
-            upload_response = requests.put(href, data=file_data)
-
+        params = {'path': self.disk_path, 'url': url_img}
+        upload_response = requests.post(upload_url, headers=self.headers, params=params)
         if upload_response.status_code in (201, 202):
-            logger.info(f"✅ Файл '{file_name}' успешно загружен в папку '{self.folder_name}'.")
+            logger.info(f"✅ Файл '{self.file_name}' успешно загружен в папку '{self.folder_name}'.")
         else:
             logger.critical(f"❌ Ошибка при загрузке файла: {upload_response.text}")
             raise Exception(f"Ошибка при загрузке файла: {upload_response.text}")
-
+        return self._get_info_file()
+    def _get_info_file(self) -> Optional[Dict[str, int]] | None:
         info_url = 'https://cloud-api.yandex.net/v1/disk/resources'
-        params = {'path': disk_path}
-        info_response = requests.get(info_url, headers=self.headers, params=params)
-
-        if info_response.status_code != 200:
+        params = {'path': self.disk_path}
+        timeout = 30
+        for _ in range(timeout):
+            info_response = requests.get(info_url, headers=self.headers, params=params)
+            if info_response.status_code == 200:
+                size = info_response.json().get('size')
+                if self.progress:
+                    self.progress.set_description("Загрузка файла")
+                    self.progress.update(1)
+                return {'name': self.file_name, 'size': size}
+            time.sleep(1)
+        else:
             logger.error(f"❌ Ошибка при получении информации о файле: {info_response.text}")
             return None
 
-        file_info = info_response.json()
-        size = file_info.get('size', 0)
-
-        if self.progress:
-            self.progress.set_description("Загрузка файла")
-            self.progress.update(1)
-
-        return {'name': file_name, 'size': size}
 
     @staticmethod
     def read_json_info() -> List[dict]:
@@ -124,48 +113,15 @@ class YandexAPI:
             progress.set_description("Сохранение информации")
             progress.update(1)
 
-
-def text_in_image_cat(text: str, save_folder: str, progress: Optional[tqdm] = None) -> str:
-    """Функция для скачивания изображений с котами с сайта cataas.com
-    с указанным текстом на картинке."""
-    logger.debug(f"Скачивание картинки с котом и текстом: {text}")
-    encoded = urllib.parse.quote(text)
-    url = f'https://cataas.com/cat/says/{encoded}'
-
-    response = requests.get(url)
-    if response.status_code != 200:
-        logger.error(f"❌ Ошибка получения изображения: {response.text}")
-
-    safe_name = text
-    for ch in ['@', '#', '%', '?', '!', ':', '"', "'", '<', '>', '/', '\\', '|', '*']:
-        safe_name = safe_name.replace(ch, '')
-
-    save_path = os.path.join(save_folder, f"{safe_name}.jpg")
-    os.makedirs(save_folder, exist_ok=True)
-
-    with open(save_path, 'wb') as f:
-        f.write(response.content)
-
-    logger.info(f"✅ Картинка с котом сохранена как: {save_path}")
-
-    if progress:
-        progress.set_description("Скачивание картинки")
-        progress.update(1)
-
-    return save_path
-
 if __name__ == "__main__":
-    save_folder = 'images'
     image_text = input('Введите надпись для изображения с котом: ')
+    url_image = rf"https://cataas.com/cat/says/{quote(image_text)}"
 
-    with tqdm(total=4, desc='Начало работы') as progress:
-        # Скачивание изображения
-        local_image_path = text_in_image_cat(image_text, save_folder, progress)
-
+    with tqdm(total=3, desc='Начало работы') as progress:
         # Работа с Яндекс Диском
         ya_disk = YandexAPI(yd_token, progress=progress)
         ya_disk.create_folder()
 
-        meta_data = ya_disk.upload_file(local_image_path)
+        meta_data = ya_disk.upload_file(url_image)
         if meta_data:
             ya_disk.save_meta_info(meta_data, progress)
