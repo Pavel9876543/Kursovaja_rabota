4d9dd04eb18c2bd6aa851ed4b2853fec4af94ce1
Pavel <gusevp220@gmail.com>
2025-08-02 12:45:58 +0300

    Улучшение по рекомендациям проверяющего преподавателя

--- images_cats.py
+++ images_cats.py
@@ -7,6 +7,7 @@ import json
 from urllib.parse import quote, unquote
 from tqdm import tqdm
 from typing import Optional, Dict, List
+import argparse
 
 # Создание собственного логгера
 logger = logging.getLogger('my_logs')
@@ -71,11 +72,16 @@ class YandexAPI:
             self.invalid_token()
         else:
             logger.error(f"❌ Ошибка при создании папки: {response.text}")
+            raise Exception(f"❌ Ошибка при создании папки: {response.text}")
 
     def upload_file(self, url_img: str) -> Optional[Dict[str, int]] | None:
         """Загрузка файла на Яндекс диск"""
         logger.debug(f"Загрузка файла {unquote(url_img)} в папку {self.folder_name} на Яндекс диске")
-        self.file_name = os.path.basename(unquote(url_img))+'.jpg'
+        self.file_name = os.path.basename(unquote(url_img))
+        for symb in ['.', ',', '?', '!', '@', '\'', '"', '\\', '/', ':', ';', '`']:
+            if symb in self.file_name:
+                self.file_name = self.file_name.replace(symb, '')
+        self.file_name = self.file_name.replace(' ', '_') + '.jpg'
         self.disk_path = f'{self.folder_name}/{self.file_name}'
         upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
         params = {'path': self.disk_path, 'url': url_img}
@@ -125,19 +131,34 @@ class YandexAPI:
             logger.info('\n'+'='*60+'\n')
 
 if __name__ == "__main__":
-    image_text = input('Введите надпись для изображения с котом: ')
-    url_image = rf"https://cataas.com/cat/says/{quote(image_text)}"
+    parser = argparse.ArgumentParser(description='Париснг надписей для изображения')
+    parser.add_argument('--inscription', nargs='+', help='Текст, содержащий несколько надписей')
+    args = parser.parse_args()
+    list_image_text = args.inscription
+    flag = not bool(list_image_text)
+    while flag:
+        image_text = input('Введите надпись для изображения с котом (или 0 для завершения ввода): ')
+        if image_text == '0':
+            break
+        list_image_text.append(image_text)
+    for img_text in list_image_text:
+        url_image = rf"https://cataas.com/cat/says/{quote(img_text)}"
 
-    with tqdm(total=3, desc='Начало работы') as progress:
-        # Работа с Яндекс Диском
-        ya_disk = YandexAPI(yd_token)
-        progress.set_description("Создание папки")
-        ya_disk.create_folder()
-        progress.update(1)
-        progress.set_description("Загрузка файла")
-        meta_data = ya_disk.upload_file(url_image)
-        progress.update(1)
-        if meta_data:
-            progress.set_description("Сохранение информации")
-            ya_disk.save_meta_info(meta_data)
+        with tqdm(total=3, desc='Начало работы') as progress:
+            # Работа с Яндекс Диском
+            ya_disk = YandexAPI(yd_token)
+            progress.set_description("Создание папки")
+            ya_disk.create_folder()
             progress.update(1)
+            progress.set_description("Загрузка файла")
+            meta_data = ya_disk.upload_file(url_image)
+            progress.update(1)
+            if meta_data:
+                progress.set_description("Сохранение информации")
+                ya_disk.save_meta_info(meta_data)
+                progress.update(1)
+    with open('meta_info.json', 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    count = len(list_image_text)
+    total_size = sum(item['size'] for item in data[-count:])
+    print(f'Загружено {count} файл(а,ов), Всего: {total_size} КБ')
